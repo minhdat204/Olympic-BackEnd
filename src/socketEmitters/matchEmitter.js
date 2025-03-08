04bea8dcc5a50e0b3d4ab9086693a657ad19e336
Dat <0306221011@caothang.edu.vn>
2025-03-08 12:39:31 +0700

    feat(socketEmitters): add match status update emitter

---
+++ src/socketEmitters/matchEmitter.js
@@ -0,0 +1,14 @@
+const { io } = require('../socketManager');
+
+// các hàm emit event
+const emitMatchStatusUpdate = async (matchId, newStatus) => {
+    console.log(`🚀 Emitting event to match_${matchId} with status: ${newStatus}`);
+    io.to(`match_${matchId}`).emit('match_status_update', {
+        matchId,
+        newStatus
+    });
+};
+
+module.exports = {
+    emitMatchStatusUpdate
+};
