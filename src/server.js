2c819a8998bfdab5557be63a504674bedccd2fcc
Dat <0306221011@caothang.edu.vn>
2025-03-08 12:36:57 +0700

    add CORS support and match routes to server configuration

--- src/server.js
+++ src/server.js
@@ -2,16 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const { sequelize } = require('./models');
 const authRoutes = require('./routes/auth');
+const matchRoutes = require('./routes/match'); 
 const http = require('http');
 const socketIo = require('socket.io');
-const initializeSocket = require('./socket');
+const { initializeSocket } = require('./socketManager');
+const cors = require('cors'); // Import thư viện cors
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: "*", // Cho phép kết nối từ bất kỳ frontend nào (hoặc sửa thành domain cụ thể)
+    methods: ["GET", "POST"]
+  }
+});
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/match', matchRoutes);
 
 initializeSocket(io);
 
