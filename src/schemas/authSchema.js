1de09faa3119f05bdca0906f8c02f80dd0e02bbc
Dat <0306221011@caothang.edu.vn>
2025-03-09 10:38:00 +0700

    feat(schema): add login schema validation using Joi

---
+++ src/schemas/authSchema.js
@@ -0,0 +1,9 @@
+// src/schemas/authSchema.js
+const Joi = require('joi');
+
+const loginSchema = Joi.object({
+  username: Joi.string().required(),         // Tên người dùng là chuỗi, bắt buộc
+  password: Joi.string().min(6).required(),  // Mật khẩu ít nhất 6 ký tự, bắt buộc
+});
+
+module.exports = { loginSchema };
