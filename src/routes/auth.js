9f89adbe2ac12b6a58e5edb0f92a4eb55d84d113
longphan2k4 <ptlong2k4@gmail.com>
2025-03-15 11:21:37 +0700

    feat(user) : add get listuser , updateuser

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authMiddleware = require('../middleware/auth');
-const roleMiddleware = require('../middleware/role');
-const AuthController = require('../controllers/authController');
-const validate = require('../middleware/validate'); // validate middleware
-const { loginSchema, registerSchema } = require('../schemas/authSchema');  // Joi schema
+const authMiddleware = require("../middleware/auth");
+const role = require("../middleware/role");
+const AuthController = require("../controllers/authController");
+const validate = require("../middleware/validate"); // validate middleware
+const { loginSchema, registerSchema } = require("../schemas/authSchema"); // Joi schema
 
 //router.use(roleMiddleware);
-router.post('/login', validate(loginSchema), AuthController.login); // khi gửi request đến /login, nó sẽ kiểm tra validate trước
-router.post('/register', validate(registerSchema), AuthController.register);
-router.post('/logout', authMiddleware, AuthController.logout);
-
+router.get("/", AuthController.getJudges);
+router.post("/login", validate(loginSchema), AuthController.login); // khi gửi request đến /login, nó sẽ kiểm tra validate trước
+router.post("/register", validate(registerSchema), AuthController.register);
+router.use(authMiddleware);
+router.post("/logout", AuthController.logout);
+router.patch("/:id", role("admin"), AuthController.updateUser);
 module.exports = router;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,50 +1,70 @@
-const { User } = require('../models');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { Op } = require('sequelize');
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { Op } = require("sequelize");
+const { use } = require("../routes/auth");
 
 const blacklistedTokens = new Set(); // Tạo một set để lưu token đã logout
 
 class AuthService {
-    // Hàm login
-    static async login(usernameOrEmail, password) {
-        const user = await User.findOne({ 
-            where: {
-                [Op.or]: [
-                    { username: usernameOrEmail },
-                    { email: usernameOrEmail }
-                ]
-            }
-        });
-        
-        if (!user || !(await bcrypt.compare(password, user.password))) {
-            throw new Error('Thông tin đăng nhập không chính xác');
-        }
-        
-        // Tạo token với id và role của user
-        const token = jwt.sign({ id: user.id, role: user.role, username: user.username, email: user.email }, process.env.JWT_SECRET, {
-            expiresIn: process.env.JWT_EXPIRES_IN || '1d',
-        });
-        
-        return { token, user };
+  // Hàm login
+  static async login(usernameOrEmail, password) {
+    const user = await User.findOne({
+      where: {
+        [Op.or]: [{ username: usernameOrEmail }, { email: usernameOrEmail }],
+      },
+    });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new Error("Thông tin đăng nhập không chính xác");
     }
-    // Hàm register
-    static async register(username, email, password) {
-        const user = await User.create({
-            username,
-            email,
-            password: await bcrypt.hash(password, 10),
-            role: 'judge',
-        });
-        return user;
+
+    // Tạo token với id và role của user
+    const token = jwt.sign(
+      {
+        id: user.id,
+        role: user.role,
+        username: user.username,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_EXPIRES_IN || "1d",
+      }
+    );
+
+    return { token, user };
+  }
+  // Hàm register
+  static async register(username, email, password) {
+    const user = await User.create({
+      username,
+      email,
+      password: await bcrypt.hash(password, 10),
+    });
+    return user;
+  }
+  // Hàm logout
+  static async logout(token) {
+    if (token) {
+      blacklistedTokens.add(token);
     }
-    // Hàm logout
-    static async logout(token) {
-        if (token) {
-            blacklistedTokens.add(token);
-        }
-        return { message: 'Logged out' };
+    return { message: "Logged out" };
+  }
+  static async getJudges() {
+    return User.findAll({
+      where: { role: "judge" },
+    });
+  }
+  static async updateUser(id, data) {
+    {
+      const user = await User.findByPk(id);
+      if (!user) throw new Error("Không tìm thấy tài khoản");
+      user.set(data);
+      await user.save();
+      return user;
     }
+  }
 }
 
 module.exports = { AuthService, blacklistedTokens };
