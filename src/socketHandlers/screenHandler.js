98756b0e33ca70a42f2ac9ab79a5c97ec9457d23
longphan2k4 <ptlong2k4@gmail.com>
2025-03-26 07:20:42 +0700

    Long realtime play video

--- src/socketHandlers/screenHandler.js
+++ src/socketHandlers/screenHandler.js
@@ -6,6 +6,10 @@ const handleScreenSokets = (io, socket) => {
     console.log(data);
     io.emit("screens", data);
   });
+  socket.on("control_audio_question_emit", (data) => {
+    console.log(data);
+    io.emit("control_audio_question_on", data);
+  });
 };
 
 module.exports = { handleScreenSokets };
